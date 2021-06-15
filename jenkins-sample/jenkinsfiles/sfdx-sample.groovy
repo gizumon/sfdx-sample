0a2a56b2a265c1bafaf30aaa74aac65f841c727a
Tomoatsu Sekikawa <gizumon@TomoatsunoAir.AirPort>
2021-06-15 14:17:30 +0900

    Add custom object and Apex test code

--- jenkins-sample/jenkinsfiles/sfdx-sample.groovy
+++ jenkins-sample/jenkinsfiles/sfdx-sample.groovy
@@ -6,6 +6,7 @@ pipeline {
         string(name: 'SFDX_USERNAME', defaultValue: 'jenkins@service.dev.com', description: 'デプロイを実行するSalesforceするユーザー名')
         string(name: 'GITLAB_URL', defaultValue: 'https://code-repo.develop.devcond-test.net/user.tomoatsu.sekikawa/sfdx-sample.git', description: 'SFDXプロジェクト Gitlab URL')
         string(name: 'DEPLOY_BRANCH', defaultValue: 'master', description: 'デプロイ対象のブランチ')
+        choice(name: 'IS_RUN_TEST', choices: ['TRUE', 'FALSE'], description: 'テストを実行するか')
         // If set this key in each build
         // string(name: 'CONSUMER_KEY', defaultValue: "${env.CONSUMER_KEY}", description: 'Salesforceへアクセスするコンシューマーキー')
     }
@@ -25,10 +26,23 @@ pipeline {
                 echo 'INFO: Build envionments of pipeline'
                 sh "git checkout -B deploy origin/${DEPLOY_BRANCH}"
                 sh '''
+                    mkdir ./release
                     node -v > ./release/build-environment.txt
                     npm -v >> ./release/build-environment.txt
                     sfdx -v >> ./release/build-environment.txt
                 '''
+                // Login Salesforce Org
+                echo "INFO: Login ${STAGE} envionments of pipeline as ${SFDX_USERNAME} user"
+                sh """
+                    if [ "${STAGE}" = "PROD" ]; then
+                        # Login PROD org
+                        sfdx force:auth:jwt:grant -i ${CONSUMER_KEY} -u ${SFDX_USERNAME} -f ${SFDX_SEVER_KEY} -a sfdx
+                    else
+                        # Login TEST org
+                        # TODO: Check actual sandbox org because there is no Sandbox in DeveloperEditon.
+                        sfdx force:auth:jwt:grant -i ${CONSUMER_KEY} -u ${SFDX_USERNAME} -f ${SFDX_SEVER_KEY} -a sfdx --instanceurl https://test.salesforce.com
+                    fi
+                """
             }
         }
         stage('Build') {
@@ -45,27 +59,21 @@ pipeline {
         stage('Test') {
             steps {
                 // INFO: If you would like to test Apex then define the followigns...
-                sh '''
-                    echo y | sfdx plugins:install sfdx-codescan-plugin
-                    sfdx force:apex:test:run
-                    sfdx force:apex:test:report
-                '''
+                sh """
+                    # echo y | sfdx plugins:install sfdx-codescan-plugin
+                    # sfdx force:apex:test:run --synchronous -w "-1" -c -v -r human --testlevel=RunLocalTests -u ${SFDX_USERNAME}
+                """
             }
         }
         stage('Deploy') {
             steps {
                 sh """
-                    if [ "${STAGE}" = "PROD" ]; then
-                        # Login PROD org
-                        sfdx force:auth:jwt:grant -i ${CONSUMER_KEY} -u ${SFDX_USERNAME} -f ${SFDX_SEVER_KEY} -a sfdx
-                    else
-                        # Login TEST org
-                        # TODO: Check actual sandbox org because there is no Sandbox in DeveloperEditon.
-                        sfdx force:auth:jwt:grant -i ${CONSUMER_KEY} -u ${SFDX_USERNAME} -f ${SFDX_SEVER_KEY} -a sfdx --instanceurl https://test.salesforce.com
-                    fi
                     # Depoly converted metadata
-                    sfdx force:mdapi:deploy -w "-1" -d ./release -u sfdx
-                    # sfdx force:mdapi:deploy -d ./release -u sfdx
+                    if [ '${IS_RUN_TEST}' = 'TRUE' ]; then
+                        sfdx force:mdapi:deploy -w '-1' -d ./release -u sfdx --testlevel=RunLocalTests
+                    else
+                        sfdx force:mdapi:deploy -w '-1' -d ./release -u sfdx --testlevel=NoTestRun
+                    fi
                 """
             }
         }
